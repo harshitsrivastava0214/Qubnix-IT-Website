12dbd9e940baa438076cb7a98f2fda1b5a7d021b
Qubnix IT Solutions <harshit@qubnixitsolutions.com>
2026-07-14 02:14:56 +0530

    website-testing-- PHASE 4

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Menu, X } from 'react-icons/fa';
 import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
