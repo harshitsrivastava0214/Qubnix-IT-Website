88494bfb02bcdbde1fb4c136cf0f9d1f0e2673e6
AniketBaghel1601 <aniket357baghel@gmail.com>
2026-07-19 01:14:09 +0530

    done with the neon db connection

--- backend/config/env.js
+++ backend/config/env.js
@@ -3,12 +3,14 @@ require("dotenv").config();
 module.exports = {
   PORT: process.env.PORT || 5000,
   CLIENT_URL: process.env.CLIENT_URL || "http://localhost:3000",
-
+  ENV: process.env.ENV,
+  DATABASE_URL: process.env.DATABASE_URL,
   DB_HOST: process.env.DB_HOST,
   DB_PORT: process.env.DB_PORT,
   DB_NAME: process.env.DB_NAME,
   DB_USER: process.env.DB_USER,
   DB_PASSWORD: process.env.DB_PASSWORD,
+
 
   EMAIL_USER: process.env.EMAIL_USER,
   EMAIL_PASSWORD: process.env.EMAIL_PASSWORD,
